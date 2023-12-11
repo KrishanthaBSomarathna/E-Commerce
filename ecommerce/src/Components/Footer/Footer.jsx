b4b1a65890f3d8dfb994625ea6d6e7992b2a8475
KrishanthaBSomarathna <info.krishantha.b@gmail.com>
2023-12-11 19:15:05 +0530

    new update

---
+++ ecommerce/src/Components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+import footer_logo from '../Assets/logo_big.png'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="footer-logo">
+            <img src={footer_logo} alt="" />
+            <p>SHOPPER</p>
+        </div>
+        <ul className='footer-link'></ul>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ ecommerce/src/Pages/LoginSign.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginSign() {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default LoginSign
